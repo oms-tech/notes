2af3199813e915df18c44180c7d7393bd31a428a
Matt Schlenker <m4ttschlenker@gmail.com>
2022-06-07 00:02:17 -0700

    Get rid of temp changes.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,62 +1,13 @@
-import { FileSystemFetcher } from "../../lib/ssg";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkStringify from "remark-stringify";
-import fs from "fs";
-import path from "path";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-const fetcher = new FileSystemFetcher();
-
-const titleMap: { [key: string]: string } = {};
-
-function addFrontmatter({ key }: { key: string }) {
-  return function transformer(tree: any) {
-    titleMap[key] = tree.children[0].children[0].value;
-  };
+type Data = {
+  name: string
 }
 
-export default async function handler(req: any, res: any) {
-  const params = await fetcher.getParams();
-
-  for (const { course, id } of params) {
-    const courseMap = {
-      "computer-networks": "Computer Networks",
-      "information-security": "Introduction to Information Security",
-      "machine-learning-trading": "Machine Learning for Trading",
-      "operating-systems": "Introduction to Operating Systems",
-      "secure-computer-systems": "Secure Computer Systems",
-      simulation: "Simulation",
-    };
-
-    const note = await fetcher.getNote({ course, id });
-    // @ts-ignore
-    const courseName = courseMap[course];
-    const key = `${course}-${id}`;
-
-    await unified()
-      .use(remarkFrontmatter)
-      .use(remarkParse)
-      .use(remarkStringify)
-      .use(addFrontmatter, { key })
-      .process(note);
-
-    const frontMatter = `---
-course: ${courseName}
-title: ${titleMap[key]}
----
-
-`;
-
-    fs.writeFile(
-      path.join("notes", course, id + ".md"),
-      frontMatter + note,
-      {},
-      (err) => {
-        if (!err) console.log("success");
-      }
-    );
-  }
-
-  res.status(200).json({ name: "John Doe" });
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: 'John Doe' })
 }
