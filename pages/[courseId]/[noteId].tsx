df4117c8775deec6a9d489cb7dbd0b788fe3e734
Matt Schlenker <m4ttschlenker@gmail.com>
2022-06-15 18:45:38 -0700

    Moves Note component into page for now.

--- pages/[courseId]/[noteId].tsx
+++ pages/[courseId]/[noteId].tsx
@@ -11,7 +11,6 @@ import remarkMath from "remark-math";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-import { Note as NoteComponent } from "../../components/Note";
 import { FileSystemFetcher } from "../../lib/ssg";
 import { isNoteMetadata, NoteParams, Note as NoteProps } from "../../types";
 
@@ -43,24 +42,39 @@ export const getStaticProps: GetStaticProps<NoteProps, NoteParams> = async ({
   return { props: { ...data, raw } };
 };
 
-const Note: NextPage<NoteProps> = ({ raw, ...metadata }) => {
-  return (
-    <main>
-      <NoteComponent {...metadata}>
-        <ReactMarkdown
-          remarkPlugins={[remarkFrontmatter, remarkMath]}
-          rehypePlugins={[
-            rehypeKatex,
-            rehypeSlug,
-            [rehypeAutolinkHeadings, { behavior: "wrap" }],
-          ]}
-          disallowedElements={["h1"]}
-        >
-          {raw}
-        </ReactMarkdown>
-      </NoteComponent>
-    </main>
-  );
-};
+const Note: NextPage<NoteProps> = ({ raw, course, title, readingTime }) => (
+  <main>
+    <div className="relative py-16 bg-white overflow-hidden">
+      <div className="relative px-4 sm:px-6 lg:px-8">
+        <div className="text-lg max-w-prose mx-auto">
+          <h1>
+            <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              {course}
+            </span>
+            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              {title}
+            </span>
+            <span className="mt-2 block text-base text-center leading-8 font-extrabold tracking-tight text-gray-300 sm:text-4xl">
+              {readingTime.text}
+            </span>
+          </h1>
+        </div>
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <ReactMarkdown
+            remarkPlugins={[remarkFrontmatter, remarkMath]}
+            rehypePlugins={[
+              rehypeKatex,
+              rehypeSlug,
+              [rehypeAutolinkHeadings, { behavior: "wrap" }],
+            ]}
+            disallowedElements={["h1"]}
+          >
+            {raw}
+          </ReactMarkdown>
+        </div>
+      </div>
+    </div>
+  </main>
+);
 
 export default Note;
